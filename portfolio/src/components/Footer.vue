59cf448735b619cf14d498a50a461c01122a74b0
Habibi <98298222+bie71@users.noreply.github.com>
2025-06-10 18:15:10 +0700

    Add portfolio/src/components/Footer.vue 1749554109610

---
+++ portfolio/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="bg-gray-800 py-6 text-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="md:flex md:items-center md:justify-between">
+        <div class="text-sm text-gray-400 order-2 md:order-1 mt-4 md:mt-0">
+          &copy; 2023 Bie77. All rights reserved.
+        </div>
+        <div class="flex justify-center space-x-4 order-1 md:order-2">
+          <a href="https://github.com/bie71" target="_blank" class="text-gray-300 hover:text-white">
+            GitHub
+          </a>
+          <a href="https://instagram.com/biie.7" target="_blank" class="text-gray-300 hover:text-white">
+            Instagram
+          </a>
+          <a href="https://facebook.com/77.bie" target="_blank" class="text-gray-300 hover:text-white">
+            Facebook
+          </a>
+          <a href="https://discord.gg/XSYM4Pvh" target="_blank" class="text-gray-300 hover:text-white">
+            Discord
+          </a>
+          <a href="https://www.linkedin.com/in/77habibi/" target="_blank" class="text-gray-300 hover:text-white">
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
