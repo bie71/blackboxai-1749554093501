833703b70cdd7b18076cf1eeec8c1f777bdcb799
Habibi <98298222+bie71@users.noreply.github.com>
2025-06-10 18:15:26 +0700

    Add portfolio/src/App.vue 1749554126252

---
+++ portfolio/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="font-sans antialiased">
+    <Navbar />
+    <HeroSection />
+    <About />
+    <Projects />
+    <Clients />
+    <Blog />
+    <ContactForm />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import HeroSection from './components/HeroSection.vue';
+import About from './components/About.vue';
+import Projects from './components/Projects.vue';
+import Clients from './components/Clients.vue';
+import Blog from './components/Blog.vue';
+import ContactForm from './components/ContactForm.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    HeroSection,
+    About,
+    Projects,
+    Clients,
+    Blog,
+    ContactForm,
+    Footer
+  }
+}
+</script>
